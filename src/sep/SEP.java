307c4493ecb928cc644c8e2198f32414d33ccefb
dennisjeff <dennis.jeffrey.t@HS-324-20.dav.edu>
2015-03-30 09:13:23 -0400

    THE MAZE
    senior exit project version 1

---
+++ src/sep/SEP.java
@@ -0,0 +1,483 @@
+package sep;
+import java.util.Scanner;
+public class SEP {
+public static void main(String[] args) {
+    System.out.println("You are about to embark on a text based adventure!");
+    System.out.println("This game is meant to make you frustrated");
+    System.out.println("Here are some simple rules");
+    System.out.println("1.In this text-based adventure type the number corresponding to the action you want to perform.");
+    System.out.println("2.This game goes by compass direction, not orientation");
+    System.out.println("3. Have FUN! (HAHAHA)");
+    System.out.println("Press 1 to begin");
+    Scanner userInput = new Scanner(System.in);
+    int start = userInput.nextInt();
+    starting_room();
+    }  
+public static void starting_room(){
+    System.out.println("\n\n\n\n\n\n\n\n");
+    System.out.println("THE MAZE");
+    System.out.println("Created by Jeffrey Dennis, Jack Frazier and Brandon Roy");
+    System.out.println("\n\n\n\n\n\n\n\n");
+    System.out.println("Before you enter we are giving you the following");
+    System.out.println("1.A Compass (To Tell Direction");
+    System.out.println("2.That's it(Hence the laughter in rule #3)");
+    System.out.println("3. Press 3 to go north and enter the maze");
+    Scanner userInput = new Scanner(System.in);
+    int start2 = userInput.nextInt();
+    if (start2 == 3){
+        room15_2();
+    }
+    else{
+        System.out.println("You did not follow the rules. That's how you lose");
+        System.out.println("GAME OVER");
+    } 
+}
+public static void transport1(){
+}
+public static void transport2(){
+}
+public static void button_room(){
+}
+public static void keyroom(){
+}
+public static void gateroom(){
+}
+public static void endroom(){
+}
+public static void room1_2(){
+}
+public static void room1_3(){
+}
+public static void room1_4(){
+}
+public static void room1_6(){
+}
+public static void room1_8(){
+}
+public static void room1_9(){
+}
+public static void room1_10(){
+}
+public static void room1_11(){
+}
+public static void room1_12(){
+}
+public static void room2_1(){
+}
+public static void room2_6(){
+}
+public static void room2_8(){
+}
+public static void room2_12(){
+}
+public static void room3_1(){
+}
+public static void room3_2(){
+}
+public static void room3_3(){
+}
+public static void room3_4(){
+}
+public static void room3_5(){
+}
+public static void room3_6(){
+}
+public static void room3_8(){
+}
+public static void room3_9(){
+}
+public static void room3_11(){
+}
+public static void room4_1(){
+}
+public static void room4_5(){
+}
+public static void room4_8(){
+}
+public static void room4_9(){
+}
+public static void room4_11(){
+}
+public static void room4_12(){
+}
+public static void room4_13(){
+}
+public static void room5_1(){
+}
+public static void room5_2(){
+}
+public static void room5_3(){
+}
+public static void room5_5(){
+}
+public static void room5_6(){
+}
+public static void room5_7(){
+}
+public static void room5_8(){
+}
+public static void room5_11(){
+}
+public static void room6_3(){
+}
+public static void room6_5(){
+}
+public static void room6_8(){
+}
+public static void room6_9(){
+}
+public static void room6_10(){
+}
+public static void room6_11(){
+}
+public static void room6_13(){
+}
+public static void room6_14(){
+}
+public static void room7_1(){
+}
+public static void room7_2(){
+}
+public static void room7_3(){
+}
+public static void room7_5(){
+}
+public static void room7_8(){
+}
+public static void room7_13(){
+}
+public static void room8_1(){
+}
+public static void room8_3(){
+}
+public static void room8_4(){
+}
+public static void room8_5(){
+}
+public static void room8_6(){
+}
+public static void room8_7(){
+}
+public static void room8_8(){
+}
+public static void room8_10(){
+}
+public static void room8_11(){
+}
+public static void room8_12(){
+}
+public static void room8_13(){
+}
+public static void room8_14(){
+}
+public static void room9_1(){
+}
+public static void room9_4(){
+}
+public static void room9_5(){
+}
+public static void room9_7(){
+}
+public static void room9_10(){
+}
+public static void room9_14(){
+}
+public static void room10_1(){
+}
+public static void room10_5(){
+}
+public static void room10_7(){
+}
+public static void room10_9(){
+}
+public static void room10_10(){
+}
+public static void room10_12(){
+}
+public static void room10_13(){
+}
+public static void room10_14(){
+}
+public static void room11_1(){
+}
+public static void room11_2(){
+}
+public static void room11_5(){
+}
+public static void room11_6(){
+}
+public static void room11_7(){
+}
+public static void room11_8(){
+}
+public static void room11_9(){
+}
+public static void room11_12(){
+}
+public static void room12_2(){
+}
+public static void room12_3(){
+}
+public static void room12_4(){
+}
+public static void room12_5(){
+}
+public static void room12_11(){
+}
+public static void room12_12(){
+}
+public static void room12_13(){
+}
+public static void room12_14(){
+}
+public static void room13_1(){
+}
+public static void room13_2(){
+}
+public static void room13_5(){
+}
+public static void room13_6(){
+}
+public static void room13_8(){
+}
+public static void room13_10(){
+}
+public static void room13_12(){
+}
+public static void room13_14(){
+}
+public static void room14_2(){
+}
+public static void room14_4(){
+}
+public static void room14_6(){
+}
+public static void room14_7(){
+}
+public static void room14_8(){
+}
+public static void room14_10(){
+}
+public static void room14_13(){
+}
+public static void room14_14(){
+}
+public static void room15_2(){
+}
+public static void room15_3(){
+}
+public static void room15_4(){
+}
+public static void room15_6(){
+}
+public static void room15_8(){
+}
+public static void room15_10(){
+}
+public static void room15_14(){
+}
+public static void room16_2(){
+}
+public static void room16_4(){
+}
+public static void room16_6(){
+}
+public static void room16_8(){
+}
+public static void room16_9(){
+}
+public static void room16_10(){
+}
+public static void room16_11(){
+}
+public static void room16_12(){
+}
+public static void room16_13(){
+}
+public static void room16_14(){
+}
+public static void room17_1(){
+}
+public static void room17_2(){
+}
+public static void room17_4(){
+}
+public static void room17_5(){
+}
+public static void room17_6(){
+}
+public static void room17_8(){
+}
+public static void room17_11(){
+}
+public static void room18_2(){
+}
+public static void room18_4(){
+}
+public static void room18_6(){
+}
+public static void room18_8(){
+}
+public static void room18_11(){
+}
+public static void room18_12(){
+}
+public static void room18_13(){
+}
+public static void room18_14(){
+}
+public static void room19_1(){
+}
+public static void room19_2(){
+}
+public static void room19_4(){
+}
+public static void room19_5(){
+}
+public static void room19_6(){
+}
+public static void room19_7(){
+}
+public static void room19_8(){
+}
+public static void room19_9(){
+}
+public static void room19_10(){
+}
+public static void room19_11(){
+}
+public static void room20_1(){
+}
+public static void room20_2(){
+}
+public static void room20_3(){
+}
+public static void room20_5(){
+}
+public static void room20_8(){
+}
+public static void room20_12(){
+}
+public static void room20_14(){
+}
+public static void room21_1(){
+}
+public static void room21_4(){
+}
+public static void room21_5(){
+}
+public static void room21_6(){
+}
+public static void room21_8(){
+}
+public static void room21_9(){
+}
+public static void room21_12(){
+}
+public static void room21_14(){
+}
+public static void room22_1(){
+}
+public static void room22_2(){
+}
+public static void room22_3(){
+}
+public static void room22_6(){
+}
+public static void room22_11(){
+}
+public static void room22_12(){
+}
+public static void room22_13(){
+}
+public static void room23_3(){
+}
+public static void room23_5(){
+}
+public static void room23_6(){
+}
+public static void room23_7(){
+}
+public static void room23_9(){
+}
+public static void room23_10(){
+}
+public static void room23_11(){
+}
+public static void room23_14(){
+}
+public static void room24_2(){
+}
+public static void room24_3(){
+}
+public static void room24_5(){
+}
+public static void room24_7(){
+}
+public static void room24_8(){
+}
+public static void room24_9(){
+}
+public static void room24_13(){
+}
+public static void room24_14(){
+}
+public static void room25_5(){
+}
+public static void room25_9(){
+}
+public static void room25_12(){
+}
+public static void room25_13(){
+}
+public static void room26_2(){
+}
+public static void room26_3(){
+}
+public static void room26_4(){
+}
+public static void room26_5(){
+}
+public static void room26_6(){
+}
+public static void room26_7(){
+}
+public static void room26_9(){
+}
+public static void room26_10(){
+}
+public static void room26_11(){
+}
+public static void room26_12(){
+}
+public static void room27_4(){
+}
+public static void room27_6(){
+}
+public static void room27_9(){
+}
+public static void room27_11(){
+}
+public static void room28_2(){
+}
+public static void room28_3(){
+}
+public static void room28_4(){
+}
+public static void room28_6(){
+}
+public static void room28_7(){
+}
+public static void room28_8(){
+}
+public static void room28_9(){
+}
+public static void room28_11(){
+}
+public static void room28_12(){
+}
+public static void room28_13(){
+}
+public static void room28_14(){
+}
+}
